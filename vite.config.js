b43846954e161faa48b798e3fa1a521a435e8aeb
yufeng.gao <yufeng.gao@tencentmusic.com>
2026-07-02 10:13:47 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/gHelper/',
+  // 区别在这里 github 需要为 /ghelper  cloudfare 不需要，部署目录不同
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
